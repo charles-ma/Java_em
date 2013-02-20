646bd1bbbb6087dd517ab5c90daa72ce4eda4e95
Chao Ma <machao@seas.upenn.edu>
2013-02-20 14:33:14 -0500

    array stacks

---
+++ Cracking/C3Q1.java
@@ -0,0 +1,15 @@
+public class C3Q1 {
+    Node<Integer>[] tops = new Node[3];
+    Node<Integer> node = new Node<Integer>();
+    int i = 0;
+    double d = i;
+    public static void main(String[] args) {
+	System.out.println("executed!");
+    }
+}
+
+class Node<T> {
+    public Node() {
+	
+    }
+} 
