03400c57eeb332ffe6cf28a2d86136aab0f0aa04
Chao Ma <machao@seas.upenn.edu>
2013-03-13 17:58:25 -0400

    C5Q12

---
+++ Cracking/C5Q1.java
@@ -0,0 +1,21 @@
+class C5Q1 {
+
+    public static void main(String[] args) {
+	int n = 356789;
+	int m = 1200023;
+	System.out.println(Integer.toBinaryString(n));
+	System.out.println(Integer.toBinaryString(m));
+	n = setBit(n, m, 2, 10);
+	System.out.println(Integer.toBinaryString(n));
+    }
+
+    public static int setBit(int n, int m, int i, int j) {
+	int mask0 = 0;
+	int mask1 = 0;
+	for(int k = 0; k < 32; k++) {
+	    if(i <= k && k <= j) mask1 += Math.pow(2, k);
+	}
+	mask0 = ~mask1;
+	return (n & mask0) + (m & mask1); 
+    }
+}
