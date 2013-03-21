3f632171bc8e6b08db4db8af307f194d56c6b302
Chao Ma <machao@seas.upenn.edu>
2013-03-21 10:08:55 -0400

    repeat

--- Cracking/C9Q5.java
+++ Cracking/C9Q5.java
@@ -1,6 +1,6 @@
 class C9Q5 {
     public static void main(String[] args) {
-	String[] a = {"ava", "ball", "", "", "call", "", "dad"};
+	String[] a = {"ava", "ball", "", "", "call", "", "dad", "", "", "", "", "", "", "", ""};
 	System.out.println(find(a, "ball"));
     }
 
@@ -9,16 +9,17 @@ class C9Q5 {
     }
 
     public static int find(String[] a, String s, int start, int end) {
-	if(start > end) return -1;
+	if(start > end) return Integer.MAX_VALUE;
 	int mid = (start + end) / 2;
-	if(a[mid].equals("")) {
-	    while(a[mid].equals("") && mid <= end) {
-		mid++;
-	    }
-	    if(mid > end) return find(a, s, start, (start + end) / 2 - 1);
+	while(mid < a.length && a[mid].equals("")) {
+	    mid++;
 	}
+	if(mid == a.length) return find(a, s, start, (start + end) / 2);
 	if(a[mid].equals(s)) return mid;
-	if(a[mid].compareTo(s) > 0) return find(a, s, start, mid - 1);
-	else return find(a, s, mid + 1, end);
+	else if(a[mid].compareTo(s) > 0){
+	    return find(a, s, start, mid - 1);
+	} else {
+	    return find(a, s, mid + 1, end);
+	}
     }
 }
--- Cracking/C9Q6.java
+++ Cracking/C9Q6.java
@@ -1,23 +1,25 @@
 class C9Q6 {
     public static void main(String[] args) {
 	int[][] a = {{1, 2, 3}, {3, 4, 6}, {4, 5, 9}};
-	find(a, 5);
+	find(a, 3);
     }
 
     /**
      * Finds a certian value in a 2d arrayx
      */
     public static void find(int[][] a, int v) {
-	int i = 0;
-	int j = a[0].length - 1;
-	while(i < a.length && j >=0 && a[i][j] != v) {
-	    if(a[i][j] < v) {
-		i++;
+	int x = 0;
+	int y = a[0].length - 1;
+	while(x < a.length && y >= 0) {
+	    if(a[x][y] == v) {
+		System.out.println("x: " + x + " y: " + y);
+		return;
+	    } else if(a[x][y] < v) {
+		x++;
 	    } else {
-		j--;
-	    } 
+		y--;
+	    }
 	}
-	if(i == a.length || j < 0) System.out.println("can't find it");
-	else System.out.println("x: " + i + " y: " + j);
+	System.out.println("cannot find this value");
     }
 }
