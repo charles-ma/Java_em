61a8e246be9a12eac2dadb4ad390737928e9e5ba
Chao Ma <machao@seas.upenn.edu>
2013-04-01 08:22:52 -0400

    C2Q2

--- Cracking/C2Q2.java
+++ Cracking/C2Q2.java
@@ -2,25 +2,30 @@ public class C2Q2 {
     public static void main(String []args) {
 	Node<Character> head = LinkedListGenerator.createCharLinkedList(10);
 	head.print();
-	System.out.println(findEle(head, 6));
+	System.out.println(findEle(head, 6).getData());
     }
 
     /**
      *Find the nth last element
      */
-    public static char findEle(Node<Character> head, int n) {
-	if(n < 0 || head == null) return ' ';
-	Node<Character> runner1 = head; 
-	Node<Character> runner2 = head; 
-	for(int i = 0; i < n; i++) {
+    public static Node<Character> findEle(Node<Character> head, int n) {
+	int i = 1;
+	Node<Character> runner = head;
+	while(i < n) {
+	    if(runner != null) {
+		runner = runner.getNext();
+	    } else {
+		break;
+	    }
+	    i++;
+	} 
+	if(i < n - 1) return null;
+	Node<Character> runner1 = head;
+	while(runner.getNext() != null) {
+	    runner = runner.getNext();
 	    runner1 = runner1.getNext();
-	    if(runner1 == null) return ' ';
 	}
-	while(runner1.getNext() != null) {
-	    runner1 = runner1.getNext();
-	    runner2 = runner2.getNext();
-	}
-	return runner2.getData();
+	return runner1;
     }
 }
 
