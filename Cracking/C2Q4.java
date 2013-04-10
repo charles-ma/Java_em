929d83773199b0d91de7185a4831df3fec73c36b
Charles Ma <mcrichard.buaa@gmail.com>
2013-04-10 08:14:51 -0400

    C2Q4

--- Cracking/C2Q4.java
+++ Cracking/C2Q4.java
@@ -5,10 +5,10 @@ public class C2Q4 {
 	head1.print();
 	head2.print();
 	addTwoLinkedLists(head1, head2, 0);
-	head2.print();
+	head1.print();
     }
-
-    public static Node<Integer> addTwoLinkedLists(Node<Integer> head1, Node<Integer> head2, int carrier) {
+    
+    /*public static Node<Integer> addTwoLinkedLists(Node<Integer> head1, Node<Integer> head2, int carrier) {
 	if(head1 == null || head2 == null) return null; 
 	int result = head1.getData() + head2.getData() + carrier;
 	head2.setData(result % 10);
@@ -20,5 +20,20 @@ public class C2Q4 {
 	}
 	addTwoLinkedLists(head1.getNext(), head2.getNext(), carrier);
 	return head2;
+	}*/
+    
+    public static Node<Integer> addTwoLinkedLists(Node<Integer> head1, Node<Integer> head2, int carrier) {
+	int result = head1.getData() + head2.getData() + carrier;
+	carrier = result / 10;
+	result = result % 10;
+	head1.setData(result);
+	if(head1.getNext() != null && head2.getNext() != null) {
+	    addTwoLinkedLists(head1.getNext(), head2.getNext(), carrier);
+	} else {
+	    if(head1.getNext() != null) head1.getNext().setData(carrier + head1.getNext().getData());
+	    else if(head2.getNext() != null) head1.setNext(new Node<Integer>(carrier + head2.getNext().getData(), null));
+	    else if(carrier == 1) head1.setNext(new Node<Integer>(carrier, null));
+	}
+	return head1;
     }
 }
