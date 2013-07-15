26d13eb80dc952cc3f5c0560ba810dfb6cdf9cd9
cma <mcrichard.buaa@gmail.com>
2013-07-15 16:19:02 -0400

    mth

--- Cracking/JTest.java
+++ Cracking/JTest.java
@@ -3,8 +3,18 @@ import java.util.*;
 public class JTest {
     public static void main(String ... args) {
 	Collection<String> cs = new ArrayList<String>();
-	for(String s : cs) {
-	    
+	cs.add("abc");
+	//String[] b = (String[]) cs.toArray();
+	Thread myThread = new Thread(new MyTask());
+	myThread.start();
+	for(int i = 0; i < 10000; ++i) {
+	    System.out.println(i);
 	}
+    }
+}
+
+class MyTask implements Runnable {
+    public void run() {
+	System.exit(0);
     }
 }
