85f5d914bffa8dce07c3063c4f73ba85236a6620
cma <mcrichard.buaa@gmail.com>
2013-07-18 16:50:25 -0400

    create instances in static block

--- Cracking/NoMain.java
+++ Cracking/NoMain.java
@@ -1,9 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class NoMain {
     public static int j;
     static {
 	int i = 0;
 	j = 1;
+	List<Integer> list = new ArrayList<Integer>();
+	list.add(1);
+	list.add(2);
 	System.out.println("There is no main function!");
+	for(int k : list) System.out.println(k);
 	System.exit(0);
     }
 }
