71baf7a440eb5d8d37d12446879e209c08d02811
cma <mcrichard.buaa@gmail.com>
2013-07-18 16:43:57 -0400

    inherit an interface multiple times

---
+++ Cracking/MultiInterface.java
@@ -0,0 +1,13 @@
+public class MultiInterface extends A implements I{
+    public static void main(String ... args) {
+	System.out.println("This class has implemented a same interface twice.");
+    }
+}
+
+class A implements I {
+
+}
+
+interface I {
+
+}
