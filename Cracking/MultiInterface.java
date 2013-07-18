fd549756fda0d4779cac807a4f303082b8cbab05
cma <mcrichard.buaa@gmail.com>
2013-07-18 17:13:09 -0400

    array type

--- Cracking/MultiInterface.java
+++ Cracking/MultiInterface.java
@@ -1,4 +1,5 @@
 public class MultiInterface extends A implements I{
+
     public static void main(String ... args) {
 	System.out.println("This class has implemented a same interface twice.");
     }
