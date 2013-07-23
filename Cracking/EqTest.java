0e8cfd6982f999a8d8e657076d643ac6f499592d
cma <mcrichard.buaa@gmail.com>
2013-07-23 09:13:11 -0400

    some equality tests

---
+++ Cracking/EqTest.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EqTest {
+    public static void main(String ... args) {
+	int a = 0;
+	Integer b = 0;
+	List<String> l = new ArrayList<String>();
+	Object obj = new Object();
+	System.out.println((a == obj) + " " + (l == obj) + " " + (a == b));
+    }
+}
