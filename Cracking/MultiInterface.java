becd3a92ed07a5a286fc49d8ac0e5e3f3eb40026
cma <mcrichard.buaa@gmail.com>
2013-07-25 10:02:32 -0400

    abstract inherit interface

--- Cracking/MultiInterface.java
+++ Cracking/MultiInterface.java
@@ -1,14 +1,21 @@
-public class MultiInterface extends A implements I{
+public class MultiInterface extends A implements I, J{
 
     public static void main(String ... args) {
 	System.out.println("This class has implemented a same interface twice.");
+	new MultiInterface().printOut();
     }
 }
 
 class A implements I {
-
+    public void printOut() {
+	System.out.println("Method from A");
+    }
 }
 
 interface I {
 
+}
+
+interface J {
+    void printOut();
 }
