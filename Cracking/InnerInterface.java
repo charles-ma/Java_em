29d188ffd3c724a762aba5dac0f8788246646b2f
cma <mcrichard.buaa@gmail.com>
2013-07-30 15:55:27 -0400

    inner interface tests

---
+++ Cracking/InnerInterface.java
@@ -0,0 +1,26 @@
+public class InnerInterface {
+
+    public static void main(String... args) {
+	A b = new InnerInterface().new B();
+	System.out.println(b.a);
+	C c = new C();
+	System.out.println(InnerInterface.B.a);
+    }
+
+    private interface A {
+	int a = 0;
+    }
+
+    public class B implements A {
+	
+    }
+}
+
+class C {
+
+    public InnerInterface.B b = new InnerInterface().new B();
+    
+    public C() {
+	System.out.println("Output from C: " + InnerInterface.B.a);
+    }
+}
