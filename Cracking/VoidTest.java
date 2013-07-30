b5dabd71e387b4624e72d88eca21f4531d2793de
cma <mcrichard.buaa@gmail.com>
2013-07-30 17:17:09 -0400

    tests for Class class

---
+++ Cracking/VoidTest.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class VoidTest {
+    public static void main(String... args) {
+	System.out.println(Void.TYPE /*instanceof Class*/);
+	List<String> l = new ArrayList<String>();
+	System.out.println(l instanceof List);
+    }
+}
