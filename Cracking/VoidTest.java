4f381b6425bafbd8b843608dfe7566a535a8edcc
cma <mcrichard.buaa@gmail.com>
2013-07-30 17:27:46 -0400

    right one

--- Cracking/VoidTest.java
+++ Cracking/VoidTest.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class VoidTest {
     public static void main(String... args) {
-	System.out.println(Void.TYPE /*instanceof Class*/);
+	System.out.println(Void.TYPE instanceof java.lang.Class);
 	List<String> l = new ArrayList<String>();
 	System.out.println(l instanceof List);
     }
