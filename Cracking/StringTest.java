e7b117fd769fe570ec213861ad21a545d5d87379
cma <mcrichard.buaa@gmail.com>
2013-07-30 15:39:15 -0400

    string tests

--- Cracking/StringTest.java
+++ Cracking/StringTest.java
@@ -4,6 +4,7 @@ public class StringTest {
 	String b = "abc";
 	String c = new String("abc");
 	String d = new String("abc");
-	System.out.println((a == b) + "/" + (a == c) + "/" + (c == d));
+	System.out.println((a == b) + "/" + (a == c) + "/" + (c == d) + "/" + (c.toString() == a) + "/" + (a.toString() == b.toString()));
+	System.out.println(new StringBuffer("abc").toString() == a);
     }
 }
