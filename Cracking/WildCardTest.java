fc8e5832049496089f42848c3c025d5ec45dec41
cma <mcrichard.buaa@gmail.com>
2013-08-01 16:40:02 -0400

    wildcard test

---
+++ Cracking/WildCardTest.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class WildCardTest {
+    public static void main(String... args) {
+	WildCardTest test = new WildCardTest();
+	test.wildFunc(new ArrayList<String>());
+    }
+
+    public void wildFunc(List<? extends Comparable> param) {
+	System.out.println("This is the wild card test!");
+    }
+}
+
