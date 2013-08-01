1c56e9c8f9a00756b8ffe2cc2aeeb15cdd849630
cma <mcrichard.buaa@gmail.com>
2013-08-01 13:44:48 -0400

    instanceof doesn't work with generic

---
+++ Cracking/ColleTest.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ColleTest {
+    public static void main(String... args) {
+	ArrayList<String> sl = new ArrayList<String>();
+	System.out.println(sl instanceof ArrayList);
+	System.out.println(sl instanceof List);
+    }
+}
