4db479bc4dd7dd7ddfb5b23b56b59b11943ac688
cma <mcrichard.buaa@gmail.com>
2013-08-01 10:38:15 -0400

    duplicate generic classes

---
+++ Cracking/DupGeneric.java
@@ -0,0 +1,13 @@
+public class DupGeneric {
+    public static void main(String... args) {
+	System.out.println("This is a duplicate class demo!");
+    }
+}
+
+class DupOne<T> {
+
+}
+
+/*class DupOne<T extends Comparable<T>> {
+
+  }*/
