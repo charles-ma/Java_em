2f7606b96401db383d4e9d690b9002d5cd4b38bf
cma <mcrichard.buaa@gmail.com>
2013-08-04 09:43:00 -0400

    vacum

--- Cracking/VoidTest.java
+++ Cracking/VoidTest.java
@@ -1,6 +1,7 @@
 import java.util.List;
 import java.util.ArrayList;
 
+
 public class VoidTest {
     public static void main(String... args) {
 	System.out.println(Void.TYPE instanceof java.lang.Class);
