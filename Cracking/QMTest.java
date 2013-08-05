cb039f37e14b7bf6796101bbdabe178b31c14683
cma <mcrichard.buaa@gmail.com>
2013-08-05 09:41:27 -0400

    tests about wild generic

--- Cracking/QMTest.java
+++ Cracking/QMTest.java
@@ -11,6 +11,12 @@ public class QMTest {
 	o.add("ads");
 	//c.add("asd");
 	System.out.println(s.get(0) + "/" + o.get(0));
+	ArrayList<ArrayList<?>> dd = new ArrayList();
+	ArrayList<ArrayList> ad = new ArrayList();
+	dd.add(a);
+	dd.add(s);
+	ad.add(a);
+	ad.add(s);
     }
     
 }
