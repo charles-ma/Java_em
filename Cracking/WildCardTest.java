dda4d642ff075ff849a309d4690acc53c4a8b1db
cma <mcrichard.buaa@gmail.com>
2013-08-06 15:17:25 -0400

    one line added

--- Cracking/WildCardTest.java
+++ Cracking/WildCardTest.java
@@ -5,6 +5,7 @@ public class WildCardTest {
     public static void main(String... args) {
 	WildCardTest test = new WildCardTest();
 	test.wildFunc(new ArrayList<String>());
+	//test.WildFunc(new ArrayList());
     }
 
     public void wildFunc(List<? extends Comparable> param) {
