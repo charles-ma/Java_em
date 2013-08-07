327d5c521d704be17580f7b23d3d5337b49f4e0f
cma <mcrichard.buaa@gmail.com>
2013-08-07 13:31:34 -0400

    small change in void test

--- Cracking/VoidTest.java
+++ Cracking/VoidTest.java
@@ -1,7 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
 
-
 public class VoidTest {
     public static void main(String... args) {
 	System.out.println(Void.TYPE instanceof java.lang.Class);
