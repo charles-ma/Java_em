70930146ffee221368238a0e30ddc165debc648a
cma <mcrichard.buaa@gmail.com>
2013-08-09 10:24:20 -0400

    native methods test

---
+++ Cracking/NativeTest.java
@@ -0,0 +1,9 @@
+public class NativeTest {
+
+    public static void main(String... args) {
+	System.out.println("This is a class with native methods!");
+    }
+    
+    public native void nativeMethod();
+    
+}
