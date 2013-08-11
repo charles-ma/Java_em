e99d68c588defbdf030a375265d031433c7d7fda
cma <mcrichard.buaa@gmail.com>
2013-08-11 09:40:19 -0400

    new ++

--- Cracking/BasicTest.java
+++ Cracking/BasicTest.java
@@ -4,9 +4,11 @@ public class BasicTest {
 	int i = 0xFFFFFFF1;
 	int j = ~i;
 	System.out.println(j);
-	int k = 0;
+	int k = 0, l = 0;
 	k = k++;
+	l = ++l;
 	System.out.println(k);
+	System.out.println(l);
     }
     
 }
