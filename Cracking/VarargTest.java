61b49353ae53cc7a3b2bb527384380fc7c376701
cma <mcrichard.buaa@gmail.com>
2013-08-13 13:13:07 -0400

    format change

--- Cracking/VarargTest.java
+++ Cracking/VarargTest.java
@@ -2,6 +2,7 @@
  * this is believed to be a compiler bug in Java 6, it was fixed in Java 7
  */
 public class VarargTest {
+
     public void v(int... vals) {
 	System.out.println("Super");
     }
@@ -9,12 +10,15 @@ public class VarargTest {
     public static void main(String... args) {
 	new SubVararg().v(1, 2, 3);
     }
+    
 }
 
 class SubVararg extends VarargTest {
+
     @Override
     public void v(int[] vals) {
 	System.out.println("Sub");
     }
+    
 }
 
