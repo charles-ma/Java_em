39682f4d5e05c57340a40f1a48dc0160ff09d41e
Charles <mcrichard.buaa@gmail.com>
2013-08-21 10:28:29 -0400

    void is also a class

--- Cracking/VoidTest.java
+++ Cracking/VoidTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class VoidTest {
     public static void main(String... args) {
 	System.out.println(Void.TYPE instanceof java.lang.Class);
+	System.out.println(Void.class instanceof java.lang.Class);
 	System.out.println(Integer.class instanceof java.lang.Class);
 	List<String> l = new ArrayList<String>();
 	System.out.println(l instanceof List);
