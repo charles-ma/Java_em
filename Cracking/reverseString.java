435f491fe58e31cd8e8fcf764b361a00877622af
Charles <mcrichard.buaa@gmail.com>
2013-08-24 09:53:54 -0400

    built-in reverse method of stringbuilder

--- Cracking/reverseString.java
+++ Cracking/reverseString.java
@@ -3,6 +3,7 @@ public class reverseString {
     public static void main(String[] args) {
 	String s = "abc";
 	System.out.println(reverse(s));
+	System.out.println(reverse1(s));
     }
 
     public static String reverse(String s) {
@@ -13,5 +14,11 @@ public class reverseString {
 	    chars[s.length() - 1 -i] = c;
 	}
 	return new String(chars);
+    }
+
+    public static String reverse1(String s) {
+	StringBuilder sb = new StringBuilder(s);
+	sb.reverse();
+	return sb.toString();
     }
 }
