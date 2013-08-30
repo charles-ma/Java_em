3cffebd9e8d83f5ab55cfda5aa42a5cd61ecc79f
Charles <mcrichard.buaa@gmail.com>
2013-08-30 10:40:54 -0400

    something about array literal

---
+++ Cracking/ArrayTest.java
@@ -0,0 +1,11 @@
+public class ArrayTest {
+    public static void main(String... args) {
+	String[] strings = new String[] {"1"};
+	mocFun(new String[]{"123"});
+	//mocFun({"123"});
+    }
+
+    public static void mocFun(String[] strings) {
+
+    }
+}
