c31ce18db9b243aa26dd23292ee1433f9d352e3a
Charles <mcrichard.buaa@gmail.com>
2013-08-30 09:20:50 -0400

    reverseStringInTwoWays

--- Cracking/C1Q2.java
+++ Cracking/C1Q2.java
@@ -1,18 +1,22 @@
 class C1Q2 {
     public static void main(String[] args) {
 	String s = "abehd";
-	System.out.println(reverse(s));
+	System.out.println(reverseBuiltIn(s));
+	System.out.println(reverseArray(s));
     }
 
-    public static String reverse(String s) {
-	if(s == null) return null;
-	char[] chars = s.toCharArray();
-	for(int i = 0; i < s.length() / 2; i++) {
-	    char c = chars[i];
-	    chars[i] = chars[s.length() - 1 - i];
-	    chars[s.length() - 1 - i] = c;
+    public static String reverseBuiltIn(String s) {
+	StringBuilder sb = new StringBuilder(s);
+	return sb.reverse().toString();
+    }
+
+    public static String reverseArray(String s) {
+	StringBuilder sb = new StringBuilder(s);
+	for (int i = 0; i < sb.length() / 2; i++) {
+	    char c = sb.charAt(sb.length() - 1 - i);
+	    sb.setCharAt(sb.length() - 1 - i, sb.charAt(i));
+	    sb.setCharAt(i, c);
 	}
-	return new String(chars);
+	return sb.toString();
     }
-
 }
