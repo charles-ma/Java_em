2051679bf7d8a06bffb831aeaf8fd73578c8d075
Charles <mcrichard.buaa@gmail.com>
2013-09-04 11:29:03 -0400

    more concise version

--- Cracking/C2Q2.java
+++ Cracking/C2Q2.java
@@ -3,6 +3,7 @@ public class C2Q2 {
 	Node<Character> head = LinkedListGenerator.createCharLinkedList(10);
 	head.print();
 	System.out.println(findEle(head, 6).getData());
+	System.out.println(findEle1(head, 6).getData());
     }
 
     /**
@@ -26,6 +27,21 @@ public class C2Q2 {
 	    runner1 = runner1.getNext();
 	}
 	return runner1;
+    }
+
+    public static Node<Character> findEle1(Node<Character> head, int n) {
+	if (head == null) return null;
+	Node<Character> runner1 = head;
+	Node<Character> runner2 = head;
+	for (int i = 0; i < n - 1; i++) {
+	    runner1 = runner1.getNext();
+	    if (runner1 == null) return null;
+	}
+	while (runner1.getNext() != null) {
+	    runner1 = runner1.getNext();
+	    runner2 = runner2.getNext();
+	}
+	return runner2;
     }
 }
 
