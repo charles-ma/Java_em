27a145582630c7ca01aa10e0dc425675e8802731
Charles <mcrichard.buaa@gmail.com>
2013-09-04 22:12:13 -0400

    clean version

--- Cracking/C2Q4.java
+++ Cracking/C2Q4.java
@@ -4,36 +4,20 @@ public class C2Q4 {
 	Node<Integer> head2 = LinkedListGenerator.createIntLinkedList(5);
 	head1.print();
 	head2.print();
-	addTwoLinkedLists(head1, head2, 0);
-	head1.print();
+	addTwoLinkedLists1(head1, head2).print();
     }
     
-    /*public static Node<Integer> addTwoLinkedLists(Node<Integer> head1, Node<Integer> head2, int carrier) {
-	if(head1 == null || head2 == null) return null; 
-	int result = head1.getData() + head2.getData() + carrier;
-	head2.setData(result % 10);
-	carrier = result / 10;
-	if(head2.getNext() == null && carrier != 0) {
-	    Node<Integer> node = new Node<Integer>(carrier, null);
-	    head2.setNext(node);
-	    return head2;
-	}
-	addTwoLinkedLists(head1.getNext(), head2.getNext(), carrier);
-	return head2;
-	}*/
-    
-    public static Node<Integer> addTwoLinkedLists(Node<Integer> head1, Node<Integer> head2, int carrier) {
-	int result = head1.getData() + head2.getData() + carrier;
-	carrier = result / 10;
-	result = result % 10;
-	head1.setData(result);
-	if(head1.getNext() != null && head2.getNext() != null) {
-	    addTwoLinkedLists(head1.getNext(), head2.getNext(), carrier);
-	} else {
-	    if(head1.getNext() != null) head1.getNext().setData(carrier + head1.getNext().getData());
-	    else if(head2.getNext() != null) head1.setNext(new Node<Integer>(carrier + head2.getNext().getData(), null));
-	    else if(carrier == 1) head1.setNext(new Node<Integer>(carrier, null));
-	}
-	return head1;
+    public static Node<Integer> addTwoLinkedLists1(Node<Integer> head1, Node<Integer> head2) {
+	return addTwoLinkedLists1(head1, head2, 0);
+    }
+
+    public static Node<Integer> addTwoLinkedLists1(Node<Integer> head1, Node<Integer> head2, int carrier) {
+	if (head1 == null && head2 == null && carrier == 0) return null;
+	int a = head1 != null ? head1.getData() : 0;
+	int b = head2 != null ? head2.getData() : 0;
+	Node<Integer> newHead = new Node<Integer>((a + b + carrier) % 10, null);
+	carrier = (a + b + carrier) / 10;
+	newHead.setNext(addTwoLinkedLists1(head1 != null ? head1.getNext() : null, head2 != null ? head2.getNext() : null, carrier));
+	return newHead;
     }
 }
