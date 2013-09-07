309c6520f130e21391bfd0bf02fd35b189210fc9
Charles <mcrichard.buaa@gmail.com>
2013-09-07 09:51:02 -0400

    graph generator

---
+++ Cracking/GraphSample.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class GraphSample {
+    private ArrayList<GraphNode> nodes = null;
+    
+    public static void main(String... args) {
+	GraphSample gs = new GraphSample();
+	gs.printNodes();
+    }
+
+    public GraphSample() {
+	nodes = new ArrayList<GraphNode>(5);
+	for (int i = 0; i < 5; i++) nodes.add(new GraphNode(i));
+	nodes.get(0).addNexts(nodes.get(1), nodes.get(2), nodes.get(4));
+	nodes.get(1).addNexts(nodes.get(3));
+	nodes.get(3).addNexts(nodes.get(2));
+	nodes.get(2).addNexts(nodes.get(4));
+    }
+    
+    public void printNodes() {
+	for (GraphNode node : nodes) node.printNode();
+    }
+}
+
+class GraphNode {
+    private ArrayList<GraphNode> nexts = null;
+    private int id = 0;
+
+    public GraphNode(int id) {
+	this.id = id;
+	nexts = new ArrayList<GraphNode>();
+    }
+    
+    public void addNexts(GraphNode... nodes) {
+	for (GraphNode node : nodes) nexts.add(node);
+    }
+
+    public int getId() {
+	return id;
+    }
+
+    public void printNode() {
+	System.out.println("Node " + id);
+	for (GraphNode next : nexts) System.out.printf("%2d", next.getId());
+	System.out.println();
+    }
+}
