7b955fc8d62301882ded435cad92bca183c20c92
Charles <mcrichard.buaa@gmail.com>
2013-09-10 09:47:59 -0400

    sth about generic array

--- Cracking/ArrayTest.java
+++ Cracking/ArrayTest.java
@@ -1,8 +1,16 @@
+import java.util.*;
+
 public class ArrayTest {
     public static void main(String... args) {
 	String[] strings = new String[] {"1"};
 	mocFun(new String[]{"123"});
 	//mocFun({"123"});
+	//Integer[] inArray = new Object[3];
+	ArrayList<String> [] a = new ArrayList[3];
+	//a[0] = new ArrayList<Integer>();
+	ArrayList al = new ArrayList();
+	al.add(2);
+	al.add("string");
     }
 
     public static void mocFun(String[] strings) {
