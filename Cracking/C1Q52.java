f6258aa8db43cc1f47502bbdf1bc85ae9ddad561
Charles <mcrichard.buaa@gmail.com>
2013-09-17 20:18:36 -0400

    multi-ways

--- Cracking/C1Q52.java
+++ Cracking/C1Q52.java
@@ -3,6 +3,7 @@ public class C1Q52 {
 	String s = "adf fe   fewef  d";
 	System.out.println(s);
 	System.out.println(encrypt(s));
+	System.out.println(encryptArray(s));
     }
     
     public static String encrypt(String s) {
@@ -12,5 +13,25 @@ public class C1Q52 {
 	    if (sb.charAt(i) == ' ') sb.replace(i, i + 1, "%20");
 	}
 	return sb.toString();
+    }
+
+    public static String encryptArray(String s) {
+	char[] a = s.toCharArray();
+	int count = 0;
+	for (int i = 0; i < a.length; i++) {
+	    if (a[i] == ' ') count++;
+	}
+	char[] b = new char[2 * count + a.length];
+	int j = 0;
+	for (int i = 0; i < a.length; i++) {
+	    if (a[i] == ' ') {
+		b[j++] = '%';
+		b[j++] = '2';
+		b[j++] = '0';
+	    } else {
+		b[j++] = a[i];
+	    }
+	}
+	return new String(b);
     }
 }
