90df8a148961de746cf9b2baa2e597ce85a738f3
Charles <mcrichard.buaa@gmail.com>
2013-09-19 18:32:16 -0400

    fighting

--- Cracking/C4Q82.java
+++ Cracking/C4Q82.java
@@ -5,6 +5,8 @@ public class C4Q82 {
 	BTree root = BTree.createInstance(10, true);
 	root.print();
 	printAllPaths(root, 12);
+	System.out.println();
+	printAllPaths1(root, 12);
     }
 
     public static void printAllPaths(BTree root, int sum) {
@@ -24,5 +26,30 @@ public class C4Q82 {
 	printPath(root.getRight(), sum - root.getValue(), values);
 	values.removeLast();
     }
+
+    public static void printAllPaths1(BTree root, int sum) {
+	printAllPaths1(root, sum, new LinkedList<Integer>());
+    }
     
+    public static void printAllPaths1(BTree root, int sum, LinkedList<Integer> values) {
+	values.add(root.getValue());
+	if (root.getRight() == null && root.getLeft() == null) {
+	    for (int i = 0; i < values.size(); i++) {
+		int s = 0;
+		for (int j = i; j < values.size(); j++) {
+		    s += values.get(j);
+		    if (s == sum) {
+			for (int k = i; k <= j; k++) System.out.printf("%2d", values.get(k));
+			System.out.println();
+		    }
+		}
+	    }
+	    values.removeLast();
+	    return;
+	}
+	if (root.getLeft() != null) printAllPaths1(root.getLeft(), sum, values);
+	if (root.getRight() != null) printAllPaths1(root.getRight(), sum, values);
+	values.removeLast();
+    }
+
 }
