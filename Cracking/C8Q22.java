43cb31fbf1f7f484a61c58bc1af0e8c2326b25d0
Charles <mcrichard.buaa@gmail.com>
2013-09-23 10:35:54 -0400

    fighting

--- Cracking/C8Q22.java
+++ Cracking/C8Q22.java
@@ -3,27 +3,31 @@ import java.util.*;
 public class C8Q22 {
     public static void main(String... args) {
 	int size = 3;
-	new C8Q22().printPath(size);
+	Set<Pair> limits = new HashSet<Pair>();
+	C8Q22 sample = new C8Q22();
+	limits.add(sample.new Pair(1, 1));
+	sample.printPath(size, limits);
     }
 
-    public void printPath(int size) {
+    public void printPath(int size, Set<Pair> limits) {
 	if (size <= 0) return;
-	printPath(size, new LinkedList<Pair>(), 0, 0);
+	printPath(size, new LinkedList<Pair>(), 0, 0, limits);
     }
 
-    public void printPath(int size, LinkedList<Pair> result, int x, int y) {
+    public void printPath(int size, LinkedList<Pair> result, int x, int y, Set<Pair> limits) {
 	Pair p = new Pair(x, y);
+	if (limits.contains(p)) return;
 	result.add(p);
 	if (x == size - 1 && y == size - 1) {
 	    for (Pair e : result) e.printPair();
 	    System.out.println();
 	}
-	if (x + 1 < size) printPath(size, result, x + 1, y);
-	if (y + 1 < size) printPath(size, result, x, y + 1);
+	if (x + 1 < size) printPath(size, result, x + 1, y, limits);
+	if (y + 1 < size) printPath(size, result, x, y + 1, limits);
 	result.removeLast();
     }
 
-    class Pair {
+    public class Pair {
 	private int x, y;
 
 	public Pair(int x, int y) {
@@ -33,6 +37,17 @@ public class C8Q22 {
 
 	public void printPair() {
 	    System.out.print("(" + x + ", " + y + ")");
+	}
+
+	@Override
+	public int hashCode() {
+	    return x * 10 + y * 20;
+	}
+
+	public boolean equals(Object o) {
+	    if (o == null || o.getClass() != Pair.class) return false;
+	    Pair other = (Pair) o;
+	    return x == other.x && y == other.y;
 	}
     }
 }
