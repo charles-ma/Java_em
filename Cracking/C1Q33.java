96d672bae7a3b8612848121b171efdc209c7ff99
Charles <mcrichard.buaa@gmail.com>
2013-09-26 10:48:20 -0400

    fighting

--- Cracking/C1Q33.java
+++ Cracking/C1Q33.java
@@ -5,17 +5,34 @@ public class C1Q33 {
 	String s = "dshfoedf";
 	System.out.println(s);
 	System.out.println(removeDup(s));
+	System.out.println(removeDup1(s));
     }
 
     public static String removeDup(String s) {
-	StringBuilder sb = new StringBuilder();
+	StringBuilder sb = new StringBuilder(s);
 	int seen = 0;
-	for (int i = 0; i < s.length(); i++) {
-	    char c = s.charAt(i);
+	int head = 0;
+	for (int i = 0; i < sb.length(); i++) {
+	    char c = sb.charAt(i);
 	    int mask = 1 << (Character.toLowerCase(c) - 97);
-	    if ((seen & mask) == 0) sb.append(c);
+	    if ((seen & mask) == 0) {
+		sb.setCharAt(head++, c);
+	    }
 	    seen |= mask;
 	}
-	return sb.toString();
+	return sb.substring(0, head);
+    }
+
+    public static String removeDup1(String s) {
+	StringBuilder sb = new StringBuilder(s);
+	int head = 0;
+	for (int i = 1; i < sb.length(); i++) {
+	    int j = 0;
+	    for (; j <= head; j++) {
+		if (sb.charAt(j) == sb.charAt(i)) break;
+	    }
+	    if (j == head + 1) sb.setCharAt(++head, sb.charAt(i));
+	}
+	return sb.substring(0, ++head);
     }
 }
