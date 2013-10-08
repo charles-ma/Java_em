174de9b123ee3b666950465caf175b5f71d0299f
Charles <mcrichard.buaa@gmail.com>
2013-10-08 17:55:52 -0400

    fighting

--- Cracking/C4Q43.java
+++ Cracking/C4Q43.java
@@ -9,6 +9,7 @@ public class C4Q43 {
 	System.out.println(i);
 	printLevel(root, i);
 	System.out.println();
+	printAll(root);
     }
 
     public static void printLevel(BTree root, int level) {
@@ -17,6 +18,26 @@ public class C4Q43 {
 	else {
 	    printLevel(root.getLeft(), level - 1);
 	    printLevel(root.getRight(), level - 1);
+	}
+    }
+
+    public static void printAll(BTree root) {
+	LinkedList<BTree> level = new LinkedList<BTree>();
+	LinkedList<BTree> q = new LinkedList<BTree>();
+	q.add(root);
+	q.add(new BTree(Integer.MIN_VALUE));
+	while (q.size() != 0) {
+	    BTree n = q.remove();
+	    if (n.getValue() == Integer.MIN_VALUE) {
+		for (BTree t : level) System.out.printf("%3d", t.getValue());
+		System.out.println();
+		level.clear();
+		if (q.size() != 0) q.add(n);
+	    } else {
+		if (n.getLeft() != null) q.add(n.getLeft());
+		if (n.getRight() != null) q.add(n.getRight());
+		level.add(n);
+	    }
 	}
     }
 }
