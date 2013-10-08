49fcb05155e848038e6b83e9f891bdf7d07ef52b
Charles <mcrichard.buaa@gmail.com>
2013-10-08 18:51:33 -0400

    fighting

--- Cracking/C4Q53.java
+++ Cracking/C4Q53.java
@@ -7,9 +7,16 @@ public class C4Q53 {
 	BTree root = BTree.createInstance(size, true);
 	root.print();
 	BTree runner = root;
+	System.out.println("In-order:");
 	while (runner != null) {
 	    System.out.println(runner.getValue());
 	    runner = getNextIn(runner);
+	}
+	System.out.println("Pre-order:");
+	runner = root;
+	while (runner != null) {
+	    System.out.println(runner.getValue());
+	    runner = getNextPre(runner);
 	}
     }
 
@@ -25,6 +32,18 @@ public class C4Q53 {
 	while (p != null && p.getRight() == n) {
 	    n = p;
 	    p = n.getParent();
+	}
+	return p;
+    }
+
+    public static BTree getNextPre(BTree n) {
+	if (n.getLeft() != null) return n.getLeft();
+	if (n.getRight() != null) return n.getRight();
+	BTree p = n.getParent();
+	while (p != null) {
+	    if (p.getLeft() == n && p.getRight() != null) return p.getRight();
+	    n = p;
+	    p = p.getParent();
 	}
 	return p;
     }
