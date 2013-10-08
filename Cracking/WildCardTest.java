323d61a21e4b46d595aa8c9d02f3b32f940bba8d
Charles <mcrichard.buaa@gmail.com>
2013-10-08 10:09:23 -0400

    fighting

--- Cracking/WildCardTest.java
+++ Cracking/WildCardTest.java
@@ -8,6 +8,17 @@ public class WildCardTest {
     public static void main(String... args) {
 	WildCardTest test = new WildCardTest();
 	test.wildFunc(new ArrayList<String>());
+	List<String> l = new ArrayList<String>();
+	String s = "abc";
+	l.add(s);
+	System.out.println(contains(l, s));
+    }
+
+    public static boolean contains(List<?> l, Object o) {
+	for (Object other : l) {
+	    if (other == o) return true;
+	}
+	return false;
     }
 
     public void wildFunc(List<? extends Comparable> param) {
