05779a7c8d69d015304dadfd25f2394662d0d2a6
Charles <mcrichard.buaa@gmail.com>
2013-10-08 11:20:41 -0400

    fighting

---
+++ Cracking/C2Q53.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class C2Q53 {
+    public static void main(String... args) {
+	int size = 10;
+	Node<Character> head = LinkedListGenerator.createCharLinkedList(10);
+	Node<Character> runner1 = head;
+	Node<Character> runner2 = head;
+	while (runner1.getNext() != null) runner1 = runner1.getNext();
+	Random r = new Random();
+	int s = r.nextInt(10);
+	for (int i = 0; i < s; i++) runner2 = runner2.getNext();
+	head.print();
+	runner1.setNext(runner2);
+	System.out.println(runner2.getData());
+	printStart(head);
+    }
+
+    public static void printStart(Node<Character> head) {
+	Node<Character> runner1 = null;
+	Node<Character> runner2 = null;
+	while (runner1 == null || runner1 != runner2) {
+	    if (runner1 == null) runner1 = head;
+	    if (runner2 == null) runner2 = head;
+	    runner1 = runner1.getNext();
+	    runner2 = runner2.getNext().getNext();
+	}
+	runner1 = head;
+	while (runner1 != runner2) {
+	    runner1 = runner1.getNext();
+	    runner2 = runner2.getNext();
+	}
+	System.out.println(runner1.getData());
+    }
+}
