fcfc62ffa93750189698bd072f221455ab3459cd
Charles <mcrichard.buaa@gmail.com>
2013-10-08 10:54:52 -0400

    fighting

--- Cracking/C2Q43.java
+++ Cracking/C2Q43.java
@@ -10,6 +10,7 @@ public class C2Q43 {
 	head1.print();
 	head2.print();
 	add(head1, head2).print();
+	add1(head1, head2).print();
     }
 
     public static Node<Integer> add(Node<Integer> head1, Node<Integer> head2) {
@@ -22,5 +23,22 @@ public class C2Q43 {
 	int v2 = head2 == null ? 0 : head2.getData();
 	int sum = v1 + v2 + c;
 	return new Node<Integer>(sum % 10, add(head1 == null ? null : head1.getNext(), head2 == null ? head2 : head2.getNext(), sum / 10));
+    }
+
+    public static Node<Integer> add1(Node<Integer> head1, Node<Integer> head2) {
+	int c = 0;
+	Node<Integer> head = new Node<Integer>(Integer.MIN_VALUE, null);
+	Node<Integer> runner = head;
+	while (head1 != null || head2 != null || c != 0) {
+	    int v1 = head1 == null ? 0 : head1.getData();
+	    int v2 = head2 == null ? 0 : head2.getData();
+	    int sum = v1 + v2 + c;
+	    c = sum / 10;
+	    runner.setNext(new Node<Integer>(sum % 10, null));
+	    runner = runner.getNext();
+	    head1 = head1 == null ? null : head1.getNext();
+	    head2 = head2 == null ? null : head2.getNext();
+	}
+	return head.getNext();
     }
 }
