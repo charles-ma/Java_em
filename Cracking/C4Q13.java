888fce04b012aa367e1e05ddaec797e45e7c054c
Charles <mcrichard.buaa@gmail.com>
2013-10-08 16:19:58 -0400

    fighting

--- Cracking/C4Q13.java
+++ Cracking/C4Q13.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class C4Q13 {
     public static void main(String... args) {
 	BTree root = BTree.createInstance(10, true);
@@ -6,8 +8,10 @@ public class C4Q13 {
 	root1.print();
 	System.out.println(isBalanced(root));
 	System.out.println(isBalanced(root1));
-	System.out.println(isBalanced(root));
-	System.out.println(isBalanced(root1));
+	System.out.println(isBalanced1(root));
+	System.out.println(isBalanced1(root1));
+	System.out.println(isBalanced2(root));
+	System.out.println(isBalanced2(root1));
     }
     
     public static boolean isBalanced(BTree root) {
@@ -45,4 +49,33 @@ public class C4Q13 {
 	}
 	return result;
     }
+
+    public static boolean isBalanced2(BTree root) {
+	Queue<TreeWrapper> q = new LinkedList<TreeWrapper>();
+	if (root == null) return true;
+	q.add(new TreeWrapper(root, 0));
+	int min = Integer.MAX_VALUE, max = 0;
+	while (q.size() != 0) {
+	    TreeWrapper tr = q.remove();
+	    BTree t = tr.tree;
+	    if (t.getLeft() == null && t.getRight() == null) {
+		min = Math.min(min, tr.depth);
+		max = Math.max(max, tr.depth);
+	    } else {
+		if (t.getLeft() != null) q.add(new TreeWrapper(t.getLeft(), tr.depth + 1));
+		if (t.getRight() != null) q.add(new TreeWrapper(t.getRight(), tr.depth + 1));
+	    }
+	}
+	return max - min < 2;
+    }
+}
+
+class TreeWrapper {
+	public BTree tree;
+	public int depth = 0;
+	
+	public TreeWrapper(BTree tree, int depth) {
+	    this.tree = tree;
+	    this.depth = depth;
+	}
 }
