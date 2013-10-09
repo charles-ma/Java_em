216de87bddc545db65f5c46fd1682f38dd777e1c
Charles <mcrichard.buaa@gmail.com>
2013-10-09 10:48:17 -0400

    fighting

---
+++ Cracking/C4Q51.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class C4Q51 {
+    public static void main(String... args) {
+	Random r = new Random();
+	int size = r.nextInt(10);
+	BTree root = BTree.createInstance(size, true);
+	root.print();
+	BTree runner = root;
+	System.out.println("In-order:");
+	while (runner != null) {
+	    System.out.printf("%3d", runner.getValue());
+	    runner = nextIn(runner);
+	}
+	System.out.println();
+	runner = root;
+	System.out.println("Pre-order:");
+	while (runner != null) {
+	    System.out.printf("%3d", runner.getValue());
+	    runner = nextPre(runner);
+	}
+	System.out.println();
+	runner = root;
+	runner = nextIn(runner);
+	System.out.println("Post-order:");
+	while (runner != null) {
+	    System.out.printf("%3d", runner.getValue());
+	    runner = nextPost(runner);
+	}
+	System.out.println();
+    }
+
+    public static BTree nextIn(BTree n) {
+	if (n.getRight() != null) {
+	    BTree r = n.getRight();
+	    while (r.getLeft() != null) {
+		r = r.getLeft();
+	    }
+	    return r;
+	} 
+	if (n.getParent() != null) {
+	    BTree p = n.getParent();
+	    if (p.getLeft() == n) return p;
+	    n = p;
+	    p = p.getParent();
+	}
+	return null;
+    }
+
+    public static BTree nextPre(BTree n) {
+	if (n.getLeft() != null) return n.getLeft();
+	if (n.getRight() != null) return n.getRight();
+	while (n.getParent() != null) {
+	    BTree p = n.getParent();
+	    if (p.getLeft() == n && p.getRight() != null) return p.getRight();
+	    n = p;
+	    p = p.getParent();
+	}
+	return null;
+    }
+
+    public static BTree nextPost(BTree n) {
+	if (n.getParent() == null) return null;
+	BTree p = n.getParent();
+	if (p.getRight() == n) return p;
+	if (p.getRight() == null) return p;
+	BTree r = p.getRight();
+	while (r.getLeft() != null) r = r.getLeft();
+	return r;
+    }
+}
