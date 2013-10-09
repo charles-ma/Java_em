0e128a13ea0d052084a50fdc8730f4729980f5a7
Charles <mcrichard.buaa@gmail.com>
2013-10-09 09:47:44 -0400

    fighting

---
+++ Cracking/CollectionToString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class CollectionToString {
+    public static void main(String... args) {
+	List<String> l = new LinkedList<String>();
+	l.add("ab");
+	l.add("hcdo");
+	l.add("fd");
+	System.out.println(l);
+    }
+}
